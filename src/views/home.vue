da67602ada8a5f55e3ad76aa8941a1a7a368b8b3
holloe <2395255043@qq.com>
2020-06-17 16:18:24 +0800

    小组项目

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,11 @@
 <template>
-<div>
-    首页
+<div class="home">
+    <!-- 欢迎 -->
+    <div class="home_header">
+      <div class="home_name">欢迎员工：陈晨熙！</div>
+    </div>
+    <!-- 开发者 -->
+    <div></div>
 </div>
 </template>
 
@@ -15,4 +20,20 @@ export default {
 }
 </script>
 <style scoped>
+.home{
+  margin: 10px;
+}
+.home_header{
+  background-color: #ffffff;
+  padding: 10px 15px;
+  margin-bottom: 15px;
+}
+.home_name{
+  padding: 15px;
+  margin-bottom: 10px;
+  font-size: 14px;
+  background-color: #F5F5F5;
+  color: #666666;
+  border-left: 5px solid #009688;
+}
 </style>
