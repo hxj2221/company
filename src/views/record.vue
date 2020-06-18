325e9ef79fab6252c9785b45f3c4af686ae95ac2
hxj2221 <66758253+hxj2221@users.noreply.github.com>
2020-06-18 08:40:38 +0800

    黄西婕

--- src/views/record.vue
+++ src/views/record.vue
@@ -1,26 +1,50 @@
 <template>
-<div>
-  <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-    <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-    <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-    <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-    <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
-  </el-tabs>
-</div>
+  <div>
+    <el-button type="primary"  style="margin-left:15px">近一个月</el-button>
+    <el-button type="primary">近一星期</el-button>
+    <el-button type="primary">当天</el-button>
+
+    <div style="float:right">
+      <el-date-picker v-model="value1" type="date" style="width:30%"></el-date-picker>-
+      <el-date-picker v-model="value1" type="date" style="width:30%"></el-date-picker>
+      <el-button type="primary">搜索</el-button>
+    </div>
+    <el-table :data="tableData" style="width: 98%;margin-left:1vw;margin-top:1vw">
+      <el-table-column prop="username" label="姓名"></el-table-column>
+      <el-table-column prop="clocktime" label="时间"></el-table-column>
+      <el-table-column prop="up_uptime" label="上午上班时间"></el-table-column>
+      <el-table-column prop="up_downtime" label="上午下班时间"></el-table-column>
+      <el-table-column prop="down_uptime" label="下午上班时间"></el-table-column>
+      <el-table-column prop="down_downtime" label="下午下班时间"></el-table-column>
+      <el-table-column prop="up" label="上午"></el-table-column>
+      <el-table-column prop="down" label="下午"></el-table-column>
+    </el-table>
+  </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        activeName: 'first'
-      };
-    },
-    methods: {
-      handleClick(tab, event) {
-        console.log(tab, event);
-      }
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          username: "小仙女",
+          clocktime: "2020-03-09",
+          up_uptime: "2020-03-09",
+          up_downtime: "2020-03-09",
+          down_uptime: "2020-03-09",
+          down_downtime: "2020-03-09",
+          up: 4,
+          down: 8
+        }
+      ]
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
     }
-  };
+  }
+};
 </script>
 <style scoped>
 </style>
